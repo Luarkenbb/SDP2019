d653792f990bf5a890a8b226c4b7fe1df86e64b7
tkm <tkmmot321@outlook.com>
2019-06-11 21:03:59 +0800

    add reorder user control

---
+++ SDP2019/SDP2019/Dialog/ReOrderDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDP2019.Dialog
+{
+    public partial class ReOrderDetail : Form
+    {
+        DBConnection conn;
+
+        public ReOrderDetail()
+        {
+            InitializeComponent();
+        }
+
+        public ReOrderDetail(int id)
+        {
+            InitializeComponent();
+            conn.OpenConnection();
+            DataRow reOrderRS = conn.ExecuteSelectQuery("Select * from reorder Where reOrderID = '" + id + "'").Rows[0];
+            DataRow SpareRS = conn.ExecuteSelectQuery("Select * from spare Where spareID = '" + reOrderRS[1].ToString() + "'").Rows[0];
+
+            tb_spareID.Text = SpareRS[0].ToString();
+            tb_quantity.Text = SpareRS[1].ToString();
+            tb_quantitySL.Text = SpareRS[2].ToString();
+            tb_desc.Text = SpareRS[3].ToString();
+            tb_reorderID.Text = reOrderRS[0].ToString();
+            tb_status.Text = reOrderRS[3].ToString();
+
+            conn.CloseConnection();
+        }
+    }
+}
---
+++ SDP2019/SDP2019/uControl/ReOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDP2019.uControl
+{
+    public partial class ReOrder : UserControl
+    {
+        DBConnection conn;
+        public ReOrder()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable getReorderTable()
+        {
+            conn.OpenConnection();
+            DataTable rs = conn.ExecuteSelectQuery("select * from reorder");
+            conn.CloseConnection();
+            return rs;
+        }
+
+
+
+    }
+
+    
+}

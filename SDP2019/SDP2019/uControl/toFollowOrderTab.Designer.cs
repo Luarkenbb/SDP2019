b12acf79d53283aaa6f5b49623fa475dc7488f42
LuarkenBB <lamchunpang17@gmail.com>
2019-06-24 16:34:35 +0800

    sss
    123

--- SDP2019/SDP2019/uControl/toFollowOrderTab.Designer.cs
+++ SDP2019/SDP2019/uControl/toFollowOrderTab.Designer.cs
@@ -31,12 +31,16 @@
             this.groupBox9 = new System.Windows.Forms.GroupBox();
             this.button25 = new System.Windows.Forms.Button();
             this.button26 = new System.Windows.Forms.Button();
-            this.listView7 = new System.Windows.Forms.ListView();
+            this.lstFollowOrder = new System.Windows.Forms.ListView();
             this.t_followOrderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.t_orderSpareID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.t_quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.t_status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.t_dealer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.t_dealerName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.t_dateCreate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.t_FollowBy = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.t_spareID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox9.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,46 +48,51 @@
             // 
             this.groupBox9.Controls.Add(this.button25);
             this.groupBox9.Controls.Add(this.button26);
-            this.groupBox9.Location = new System.Drawing.Point(574, 6);
+            this.groupBox9.Location = new System.Drawing.Point(656, 6);
             this.groupBox9.Name = "groupBox9";
-            this.groupBox9.Size = new System.Drawing.Size(191, 79);
+            this.groupBox9.Size = new System.Drawing.Size(109, 100);
             this.groupBox9.TabIndex = 26;
             this.groupBox9.TabStop = false;
             this.groupBox9.Text = "Data control";
             // 
             // button25
             // 
-            this.button25.Location = new System.Drawing.Point(7, 48);
+            this.button25.Location = new System.Drawing.Point(6, 48);
             this.button25.Name = "button25";
-            this.button25.Size = new System.Drawing.Size(178, 23);
+            this.button25.Size = new System.Drawing.Size(94, 28);
             this.button25.TabIndex = 1;
             this.button25.Text = "Delete";
             this.button25.UseVisualStyleBackColor = true;
             // 
             // button26
             // 
-            this.button26.Location = new System.Drawing.Point(7, 19);
+            this.button26.Location = new System.Drawing.Point(6, 19);
             this.button26.Name = "button26";
-            this.button26.Size = new System.Drawing.Size(178, 23);
+            this.button26.Size = new System.Drawing.Size(94, 28);
             this.button26.TabIndex = 0;
             this.button26.Text = "Search";
             this.button26.UseVisualStyleBackColor = true;
             // 
-            // listView7
+            // lstFollowOrder
             // 
-            this.listView7.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lstFollowOrder.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.t_followOrderID,
             this.t_orderSpareID,
+            this.t_spareID,
             this.t_quantity,
             this.t_status,
-            this.t_dealer});
-            this.listView7.HideSelection = false;
-            this.listView7.Location = new System.Drawing.Point(1, 0);
-            this.listView7.Name = "listView7";
-            this.listView7.Size = new System.Drawing.Size(567, 399);
-            this.listView7.TabIndex = 25;
-            this.listView7.UseCompatibleStateImageBehavior = false;
-            this.listView7.View = System.Windows.Forms.View.Details;
+            this.t_FollowBy,
+            this.t_dealer,
+            this.t_dealerName,
+            this.t_dateCreate});
+            this.lstFollowOrder.HideSelection = false;
+            this.lstFollowOrder.Location = new System.Drawing.Point(1, 0);
+            this.lstFollowOrder.Name = "lstFollowOrder";
+            this.lstFollowOrder.Size = new System.Drawing.Size(655, 399);
+            this.lstFollowOrder.TabIndex = 25;
+            this.lstFollowOrder.UseCompatibleStateImageBehavior = false;
+            this.lstFollowOrder.View = System.Windows.Forms.View.Details;
+            this.lstFollowOrder.SelectedIndexChanged += new System.EventHandler(this.lstFollowOrder_SelectedIndexChanged);
             // 
             // t_followOrderID
             // 
@@ -93,7 +102,7 @@
             // t_orderSpareID
             // 
             this.t_orderSpareID.Text = "order SpareID";
-            this.t_orderSpareID.Width = 87;
+            this.t_orderSpareID.Width = 81;
             // 
             // t_quantity
             // 
@@ -107,14 +116,33 @@
             // 
             this.t_dealer.Text = "DealerID";
             // 
+            // t_dealerName
+            // 
+            this.t_dealerName.Text = "Dealer Name";
+            this.t_dealerName.Width = 81;
+            // 
+            // t_dateCreate
+            // 
+            this.t_dateCreate.Text = "Date Created";
+            this.t_dateCreate.Width = 134;
+            // 
+            // t_FollowBy
+            // 
+            this.t_FollowBy.Text = "FollowBy";
+            // 
+            // t_spareID
+            // 
+            this.t_spareID.Text = "SpareID";
+            // 
             // toFollowOrderTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupBox9);
-            this.Controls.Add(this.listView7);
+            this.Controls.Add(this.lstFollowOrder);
             this.Name = "toFollowOrderTab";
             this.Size = new System.Drawing.Size(767, 399);
+            this.Load += new System.EventHandler(this.toFollowOrderTab_Load);
             this.groupBox9.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -125,11 +153,15 @@
         private System.Windows.Forms.GroupBox groupBox9;
         private System.Windows.Forms.Button button25;
         private System.Windows.Forms.Button button26;
-        private System.Windows.Forms.ListView listView7;
+        private System.Windows.Forms.ListView lstFollowOrder;
         private System.Windows.Forms.ColumnHeader t_followOrderID;
         private System.Windows.Forms.ColumnHeader t_orderSpareID;
         private System.Windows.Forms.ColumnHeader t_quantity;
         private System.Windows.Forms.ColumnHeader t_status;
         private System.Windows.Forms.ColumnHeader t_dealer;
+        private System.Windows.Forms.ColumnHeader t_dealerName;
+        private System.Windows.Forms.ColumnHeader t_dateCreate;
+        private System.Windows.Forms.ColumnHeader t_FollowBy;
+        private System.Windows.Forms.ColumnHeader t_spareID;
     }
 }
--- SDP2019/SDP2019/uControl/toFollowOrderTab.cs
+++ SDP2019/SDP2019/uControl/toFollowOrderTab.cs
@@ -12,9 +12,49 @@ namespace SDP2019.uControl
 {
     public partial class toFollowOrderTab : UserControl
     {
+        DBConnection conn;
         public toFollowOrderTab()
         {
             InitializeComponent();
+        }
+
+        private void toFollowOrderTab_Load(object sender, EventArgs e)
+        {
+            conn = new DBConnection();
+            getAllToFollowOrder();
+        }
+
+        private void lstFollowOrder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getAllToFollowOrder()
+        {
+            string sql = "";
+            sql += "SELECT tofolloworderspare.followOrderID, tofolloworderspare.orderSpareID, orderspare.spareID ,tofolloworderspare.quantity, tofolloworderspare.status, tofolloworderspare.followBy, orderlist.dealerID, dealer.name, tofolloworderspare.createdDate";
+            sql += " FROM tofolloworderspare, orderlist, dealer,orderspare";
+            sql += " WHERE tofolloworderspare.orderSpareID = orderspare.orderSpareID AND orderspare.orderSerial = orderlist.orderSerial";
+            sql += " AND orderlist.dealerID = dealer.dealerID";
+            getToFollowOrderList(sql);
+
+        }
+
+        private void getToFollowOrderList(string sql)
+        {
+            conn.OpenConnection();
+
+            DataTable dt = conn.ExecuteSelectQuery(sql);
+            foreach (DataRow row in dt.Rows)
+            {
+                ListViewItem item = new ListViewItem(row[0].ToString());
+                for (int i = 1; i < dt.Columns.Count; i++)
+                {
+                    item.SubItems.Add(row[i].ToString());
+                }
+                lstFollowOrder.Items.Add(item);
+            }
+            conn.CloseConnection();
         }
     }
 }

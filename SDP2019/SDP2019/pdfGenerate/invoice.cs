ed63a113f065f031bcdb4457be99937d5a606b6b
LuarkenBB <lamchunpang17@gmail.com>
2019-06-26 21:02:12 +0800

    invoice class for generate invoice pdf
    create a class

---
+++ SDP2019/SDP2019/pdfGenerate/invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP2019.pdfGenerate
+{
+    class invoice
+    {
+        public invoice()
+        {
+
+        }
+        public invoice(string orderSerial)
+        {
+
+        }
+    }
+}
